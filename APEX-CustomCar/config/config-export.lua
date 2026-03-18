fd4c287fe5703833085508d3b406081f7c5e48f6
richardcomeback1-cloud <richardcomeback1@gmail.com>
2026-03-19 03:39:31 +0700

    Add files via upload

---
+++ APEX-CustomCar/config/config-export.lua
@@ -0,0 +1,13 @@
+Config = Config or {}
+
+Config.ScriptName = 'APEX-CustomCar'
+
+Config.ExportResources = {
+    esExtended = 'es_extended',
+    garage = 'APEX-Garage',
+    textUI = 'val-textui',
+    notify = 'mythic_notify',
+    carHUD = 'lizz_carhud',
+    playerHUD = 'lizz_playerhud',
+    serverLogs = 'azael_dc-serverlogs'
+}
